651d3e9035c3d59c5b6b902d1145235636d67fd6
Alexander Ewetumo <trinoxf@gmail.com>
2016-02-29 17:33:12 +0100

    Updated tests to pass for logger

--- log/log.go
+++ log/log.go
@@ -8,6 +8,18 @@ import (
 	"sync"
 )
 
+//==============================================================================
+
+// Log provides a log interface for the log package.
+// type Log interface {
+// 	Dev(interface{}, string, string, ...interface{})
+// 	User(interface{}, string, string, ...interface{})
+// 	Fatal(interface{}, string, string, ...interface{})
+// 	Error(interface{}, string, string, ...interface{})
+// }
+
+//==============================================================================
+
 // Level constants that define the supported usable LogLevel.
 const (
 	NONE int = iota
@@ -15,26 +27,101 @@ const (
 	USER
 )
 
-// l contains a standard logger for all logging.
-var l struct {
+//==============================================================================
+
+// Logger contains a standard logger for all logging.
+type Logger struct {
 	*log.Logger
 	level func() int
 	mu    sync.RWMutex
 }
 
+// New returns a instance of a logger.
+func New(w io.Writer, level func() int) *Logger {
+	lm := Logger{
+		Logger: log.New(w, "", log.Ldate|log.Ltime|log.Lshortfile),
+		level:  level,
+	}
+
+	return &lm
+}
+
+// Dev logs trace information for developers.
+func (l *Logger) Dev(context interface{}, funcName string, format string, a ...interface{}) {
+	l.mu.RLock()
+	{
+		if l.level() == DEV {
+			if a != nil {
+				format = fmt.Sprintf(format, a...)
+			}
+
+			l.Output(2, fmt.Sprintf("DEV : %s : %s : %s", context, funcName, format))
+		}
+	}
+	l.mu.RUnlock()
+}
+
+// User logs trace information for users.
+func (l *Logger) User(context interface{}, funcName string, format string, a ...interface{}) {
+	l.mu.RLock()
+	{
+		if l.level() >= DEV {
+			if a != nil {
+				format = fmt.Sprintf(format, a...)
+			}
+
+			l.Output(2, fmt.Sprintf("USER : %s : %s : %s", context, funcName, format))
+		}
+	}
+	l.mu.RUnlock()
+}
+
+// Error logs trace information that are errors.
+func (l *Logger) Error(context interface{}, funcName string, err error, format string, a ...interface{}) {
+	l.mu.RLock()
+	{
+		if l.level() >= DEV {
+			if a != nil {
+				format = fmt.Sprintf(format, a...)
+			}
+
+			l.Output(2, fmt.Sprintf("ERROR : %s : %s : %s : %s", context, funcName, err, format))
+		}
+	}
+	l.mu.RUnlock()
+}
+
+// Fatal logs trace information for users and terminates the app.
+func (l *Logger) Fatal(context interface{}, funcName string, format string, a ...interface{}) {
+	l.mu.RLock()
+	{
+		if l.level() >= DEV {
+			if a != nil {
+				format = fmt.Sprintf(format, a...)
+			}
+
+			l.Output(2, fmt.Sprintf("FATAL : %s : %s : %s", context, funcName, format))
+		}
+	}
+	l.mu.RUnlock()
+
+	os.Exit(1)
+}
+
 //==============================================================================
 
-// Init must be called to initialize the logging system. This function should
-// only be called once.
-func Init(w io.Writer, level func() int) error {
+// l defines the default log variable for the global log functions.
+var l Logger
+
+// Init initializes the default logger to allow usage of the global log
+// functions.
+func Init(w io.Writer, level func() int) {
 	l.mu.Lock()
 	{
 		l.Logger = log.New(w, "", log.Ldate|log.Ltime|log.Lshortfile)
 		l.level = level
 	}
 	l.mu.Unlock()
-
-	return nil
 }
 
 // Dev logs trace information for developers.
@@ -98,3 +185,5 @@ func Fatal(context interface{}, funcName string, format string, a ...interface{}
 
 	os.Exit(1)
 }
+
+//==============================================================================
--- log/log_test.go
+++ log/log_test.go
@@ -94,3 +94,67 @@ func TestLogLevelDEV(t *testing.T) {
 		}
 	}
 }
+
+// TestLogInstanceInDev tests the basic functioning of the logger instance
+func TestLogInstanceInDev(t *testing.T) {
+	t.Log("Given the need to log DEV and DEV messages.")
+	{
+		t.Log("\tWhen we set the logging level to DEV.")
+		{
+			lg := log.New(&logdest, func() int { return log.DEV })
+
+			resetLog()
+			defer displayLog()
+
+			dt := time.Now().Format("2006/01/02 15:04:05")
+
+			log1 := fmt.Sprintf("%s log_test.go:115: DEV : context : FuncName : Message 1 no format\n", dt)
+			log2 := fmt.Sprintf("%s log_test.go:116: USER : context : FuncName : Message 2 with format: A, B\n", dt)
+			log3 := fmt.Sprintf("%s log_test.go:117: ERROR : context : FuncName : An error : Message 3 with format: C, D\n", dt)
+
+			lg.Dev("context", "FuncName", "Message 1 no format")
+			lg.User("context", "FuncName", "Message 2 with format: %s, %s", "A", "B")
+			lg.Error("context", "FuncName", errors.New("An error"), "Message 3 with format: %s, %s", "C", "D")
+
+			if logdest.String() == log1+log2+log3 {
+				t.Logf("\t\t%v : Should log the expected trace line.", Success)
+			} else {
+				t.Log("***>", logdest.String())
+				t.Log("***>", log1+log2+log3)
+				t.Errorf("\t\t%v : Should log the expected trace line.", Failed)
+			}
+		}
+	}
+}
+
+// TestLogInstanceInUser tests the basic functioning of the logger instance
+func TestLogInstanceInUser(t *testing.T) {
+	t.Log("Given the need to log DEV and USER messages.")
+	{
+		t.Log("\tWhen we set the logging level to USER.")
+		{
+			// log.Init(&logdest, func() int { return log.USER })
+			lg := log.New(&logdest, func() int { return log.USER })
+
+			resetLog()
+			defer displayLog()
+
+			dt := time.Now().Format("2006/01/02 15:04:05")
+
+			log1 := fmt.Sprintf("%s log_test.go:148: USER : context : FuncName : Message 2 with format: A, B\n", dt)
+			log2 := fmt.Sprintf("%s log_test.go:149: ERROR : context : FuncName : An error : Message 3 no format\n", dt)
+
+			lg.Dev("context", "FuncName", "Message 1 no format")
+			lg.User("context", "FuncName", "Message 2 with format: %s, %s", "A", "B")
+			lg.Error("context", "FuncName", errors.New("An error"), "Message 3 no format")
+
+			if logdest.String() == log1+log2 {
+				t.Logf("\t\t%v : Should log the expected trace line.", Success)
+			} else {
+				t.Log("***>", logdest.String())
+				t.Log("***>", log1+log2)
+				t.Errorf("\t\t%v : Should log the expected trace line.", Failed)
+			}
+		}
+	}
+}
