e9d81a096fcf88b4c90bb03436014707cb0b5e0a
Alexander Ewetumo <trinoxf@gmail.com>
2016-02-29 21:32:19 +0100

    Revised addition of one to log use

--- log/log_default.go
+++ log/log_default.go
@@ -46,25 +46,25 @@ func Fatal(context interface{}, funcName string, format string, a ...interface{}
 // DevOffset logs trace information for developers with a offset option to
 // expand the caller level.
 func DevOffset(context interface{}, offset int, funcName string, format string, a ...interface{}) {
-	l.DevOffset(context, offset, funcName, format, a...)
+	l.DevOffset(context, offset+1, funcName, format, a...)
 }
 
 // UserOffset logs trace information for users with a offset option to expand the
 // caller level.
 func UserOffset(context interface{}, offset int, funcName string, format string, a ...interface{}) {
-	l.UserOffset(context, offset, funcName, format, a...)
+	l.UserOffset(context, offset+1, funcName, format, a...)
 }
 
 // ErrorOffset logs trace information that are errors with a offset option to
 // expand the caller level.
 func ErrorOffset(context interface{}, offset int, funcName string, err error, format string, a ...interface{}) {
-	l.ErrorOffset(context, offset, funcName, err, format, a...)
+	l.ErrorOffset(context, offset+1, funcName, err, format, a...)
 }
 
 // FatalOffset logs trace information for users and terminates the app with a
 // offset expand the caller level.
 func FatalOffset(context interface{}, offset int, funcName string, format string, a ...interface{}) {
-	l.FatalOffset(context, offset, funcName, format, a...)
+	l.FatalOffset(context, offset+1, funcName, format, a...)
 }
 
 //==============================================================================
