d2e164d2f6e69f6d80323ca4f3c03499511227aa
Alexander Ewetumo <trinoxf@gmail.com>
2016-03-01 07:06:00 +0100

    Fixed and added offset tests

--- log/log_test.go
+++ log/log_test.go
@@ -158,3 +158,64 @@ func TestLogInstanceInUser(t *testing.T) {
 		}
 	}
 }
+
+// TestLogLevelDEVOffset tests the basic functioning of the logger in DEV mode.
+func TestLogLevelDEVOffset(t *testing.T) {
+	t.Log("Given the need to log DEV and USER messages.")
+	{
+		t.Log("\tWhen we set the logging level to DEV.")
+		{
+			log.Init(&logdest, func() int { return log.DEV })
+			resetLog()
+			defer displayLog()
+
+			dt := time.Now().Format("2006/01/02 15:04:05")
+
+			log1 := fmt.Sprintf("%s log_test.go:178: DEV : context : FuncName : Message 1 no format\n", dt)
+			log2 := fmt.Sprintf("%s log_test.go:179: USER : context : FuncName : Message 2 with format: A, B\n", dt)
+			log3 := fmt.Sprintf("%s log_test.go:180: ERROR : context : FuncName : An error : Message 3 with format: C, D\n", dt)
+
+			log.DevOffset("context", 0, "FuncName", "Message 1 no format")
+			log.UserOffset("context", 0, "FuncName", "Message 2 with format: %s, %s", "A", "B")
+			log.ErrorOffset("context", 0, "FuncName", errors.New("An error"), "Message 3 with format: %s, %s", "C", "D")
+
+			if logdest.String() == log1+log2+log3 {
+				t.Logf("\t\t%v : Should log the expected trace line.", Success)
+			} else {
+				t.Log("***>", logdest.String())
+				t.Log("***>", log1+log2+log3)
+				t.Errorf("\t\t%v : Should log the expected trace line.", Failed)
+			}
+		}
+	}
+}
+
+// TestLogLevelUserOffset tests the basic functioning of the logger in DEV mode.
+func TestLogLevelUserOffset(t *testing.T) {
+	t.Log("Given the need to log DEV and USER messages.")
+	{
+		t.Log("\tWhen we set the logging level to DEV.")
+		{
+			log.Init(&logdest, func() int { return log.USER })
+			resetLog()
+			defer displayLog()
+
+			dt := time.Now().Format("2006/01/02 15:04:05")
+
+			log1 := fmt.Sprintf("%s log_test.go:209: USER : context : FuncName : Message 2 with format: A, B\n", dt)
+			log2 := fmt.Sprintf("%s log_test.go:210: ERROR : context : FuncName : An error : Message 3 with format: C, D\n", dt)
+
+			log.DevOffset("context", 0, "FuncName", "Message 1 no format")
+			log.UserOffset("context", 0, "FuncName", "Message 2 with format: %s, %s", "A", "B")
+			log.ErrorOffset("context", 0, "FuncName", errors.New("An error"), "Message 3 with format: %s, %s", "C", "D")
+
+			if logdest.String() == log1+log2 {
+				t.Logf("\t\t%v : Should log the expected trace line.", Success)
+			} else {
+				t.Log("***>", logdest.String())
+				t.Log("***>", log1+log2)
+				t.Errorf("\t\t%v : Should log the expected trace line.", Failed)
+			}
+		}
+	}
+}
