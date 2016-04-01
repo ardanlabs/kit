d64753bde29a596117fae54d2b6ecce3e7b61c32
William Kennedy <bill@ardanlans.com>
2016-04-01 11:16:05 -0400

    Fixes runner code.

--- runner/runner.go
+++ runner/runner.go
@@ -5,12 +5,10 @@ package runner
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/signal"
-	"sync"
 	"time"
-
-	"github.com/ardanlabs/kit/log"
 )
 
 // Error variables for the different states.
@@ -25,81 +23,63 @@ type Jobber interface {
 	Job(context interface{}) error
 }
 
-// runner maintains state for the running process.
-var runner struct {
-	sync.Mutex
-
+// Runner maintains state for the running process.
+type Runner struct {
 	shutdown chan struct{}
 	sigChan  chan os.Signal
 	kill     <-chan time.Time
 	complete chan error
+}
 
-	recvShutdown bool
+// New returns a new Runner value for use.
+func New(timeout time.Duration) *Runner {
+	return &Runner{
+		shutdown: make(chan struct{}),
+		sigChan:  make(chan os.Signal, 1),
+		kill:     time.After(timeout),
+		complete: make(chan error),
+	}
 }
 
 // Run performs the execution of the specified job.
-func Run(context interface{}, timeout time.Duration, job Jobber) error {
-	log.User(context, "Run", "Started : Timeout[%v]", timeout)
-
-	// Init the runner for use.
-	if initRunner(timeout) {
-		return errors.New("Already running")
-	}
-
-	// When the task is done reset everything.
-	defer resetRunner()
+func (r *Runner) Run(context interface{}, job Jobber) error {
 
 	// We want to receive all interrupt based signals.
-	signal.Notify(runner.sigChan, os.Interrupt)
+	signal.Notify(r.sigChan, os.Interrupt)
 
 	// Launch the processor.
-	go processor(context, job)
+	go r.processor(context, job)
 
 	for {
 		select {
-		case <-runner.sigChan:
-
-			// Interrupt event signaled by the operation system.
-			log.User(context, "Run", "Interrupt Received")
-
-			// Flag we received the signal.
-			runner.recvShutdown = true
+		case <-r.sigChan:
 
 			// Close the channel to signal to the processor
 			// it needs to shutdown.
-			close(runner.shutdown)
+			close(r.shutdown)
 
 			// No need to process anymore events.
-			signal.Stop(runner.sigChan)
+			signal.Stop(r.sigChan)
 
-		case <-runner.kill:
+		case <-r.kill:
 
 			// We have taken too much time. Kill the app hard.
-			log.User(context, "Run", "Completed : Timedout")
 			return ErrTimeout
 
-		case err := <-runner.complete:
+		case err := <-r.complete:
 
-			// Everything completed within the time given.
-			log.User(context, "Run", "Completed : Task Result : %v", err)
-
-			if runner.recvShutdown {
-				return ErrSignaled
-			}
-
+			// Return the potential error.
 			return err
 		}
 	}
 }
 
-// CheckShutdown checks the shutdown flag to determine
-// if we have been asked to interrupt processing.
-func CheckShutdown(context interface{}) bool {
+// CheckShutdown can be used to check if a shutdown request has been issued.
+func (r *Runner) CheckShutdown() bool {
 	select {
-	case <-runner.shutdown:
+	case <-r.shutdown:
 
 		// We have been asked to shutdown.
-		log.User(context, "CheckShutdown", "Shutdown Early")
 		return true
 
 	default:
@@ -109,39 +89,8 @@ func CheckShutdown(context interface{}) bool {
 	}
 }
 
-// resetRunner allows the runner to run a new task.
-func resetRunner() {
-	runner.Lock()
-	{
-		runner.shutdown = nil
-		runner.sigChan = nil
-		runner.kill = nil
-		runner.complete = nil
-		runner.recvShutdown = false
-	}
-	runner.Unlock()
-}
-
-// initRunner will check if a task is already running. If not
-// it will initialize the runner to run a task.
-func initRunner(timeout time.Duration) bool {
-	runner.Lock()
-	defer runner.Unlock()
-
-	if runner.shutdown == nil {
-		runner.shutdown = make(chan struct{})
-		runner.sigChan = make(chan os.Signal, 1)
-		runner.kill = time.After(timeout)
-		runner.complete = make(chan error)
-		return false
-	}
-
-	return true
-}
-
 // processor provides the main program logic for the program.
-func processor(context interface{}, job Jobber) {
-	log.User(context, "processor", "Started")
+func (r *Runner) processor(context interface{}, job Jobber) {
 
 	// Variable to store any error that occurs.
 	var err error
@@ -152,15 +101,13 @@ func processor(context interface{}, job Jobber) {
 
 		// Capture any potential panic.
 		if r := recover(); r != nil {
-			log.User(context, "processor", "Panic : %v", r)
+			err = fmt.Errorf("%v", r)
 		}
 
 		// Signal the goroutine we have shutdown.
-		runner.complete <- err
+		r.complete <- err
 	}()
 
 	// Run the job.
 	err = job.Job(context)
-
-	log.User(context, "processor", "Completed")
 }
