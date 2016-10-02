cd7b2ace1a43db601fb6388932e5d78797a8a9f9
William Kennedy <bill@ardanlabs.com>
2016-10-02 05:27:41 +0100

    Minor spacing for readability

--- web/app/context.go
+++ web/app/context.go
@@ -184,11 +184,13 @@ func (c *Context) Proxy(targetURL string, rewrite func(req *http.Request)) error
 func singleJoiningSlash(a, b string) string {
 	aslash := strings.HasSuffix(a, "/")
 	bslash := strings.HasPrefix(b, "/")
+
 	switch {
 	case aslash && bslash:
 		return a + b[1:]
 	case !aslash && !bslash:
 		return a + "/" + b
 	}
+
 	return a + b
 }
